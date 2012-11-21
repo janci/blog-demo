7d8eb3fecb47e4fd45a0931aa56f41ddf16f9cd5
Jan Svantner <janci@janci.net>
2012-11-21 19:48:19 +0100

    add Email send to KontaktPresenter

--- app/presenters/KontaktPresenter.php
+++ app/presenters/KontaktPresenter.php
@@ -15,7 +15,33 @@ class KontaktPresenter extends BasePresenter
         $form['message']->addRule(Form::FILLED, 'Správa je povinný údaj.');
         $form['email']->addRule(Form::EMAIL, 'Email nemá správny formát');
 
+        $form->onSuccess[] = array($this, 'sendEmail');
+
         return $form;
+    }
+
+    public function sendEmail(\Nette\Application\UI\Form $form) {
+        $values = $form->getValues();
+
+        $options = array(
+            'host' => 'smtp.gmail.com',
+            'username' => 'prednaska@janci.net',
+            'password' => 'mrkvaAHrasok',
+            'secure' => 'ssl'
+        );
+
+        $html = '<i>'.$values->message.'</i><br /><br />'.$values->name;
+
+        $email = new \Nette\Mail\Message();
+        $email->setMailer(new \Nette\Mail\SmtpMailer($options));
+        $email->setFrom('prednaska@janci.net');
+        $email->addReplyTo($values->email);
+        $email->setSubject('Kontakt z blog.sk');
+        $email->addTo('posta.janci@gmail.com');
+        $email->setHtmlBody($html);
+        $email->send();
+
+        $this->redirect('this');
     }
 
 }
